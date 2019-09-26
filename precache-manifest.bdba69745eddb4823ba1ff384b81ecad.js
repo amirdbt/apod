b95d183293a585367d0ffabbbad918953de1d45a
amirdbt <hassanamir643@yahoo.com>
2019-09-26 18:42:30 +0100

    Updates

---
+++ precache-manifest.bdba69745eddb4823ba1ff384b81ecad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f189fdecf49efd8b5a8c21e65cbec9ca",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "a0f550923105f85f3ec4",
+    "url": "/nasa/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1a4781bedf98a0b8c1e1",
+    "url": "/nasa/static/css/main.1091dd15.chunk.css"
+  },
+  {
+    "revision": "a0f550923105f85f3ec4",
+    "url": "/nasa/static/js/2.09123ffe.chunk.js"
+  },
+  {
+    "revision": "1a4781bedf98a0b8c1e1",
+    "url": "/nasa/static/js/main.1b898f52.chunk.js"
+  },
+  {
+    "revision": "27ba3136a1915ed8c610",
+    "url": "/nasa/static/js/runtime-main.9638db3d.js"
+  }
+]);
